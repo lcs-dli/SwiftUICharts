8d979f70909655a508220028fc1784dfdff3d025
David Li <dtli@lcs.on.ca>
2024-01-27 14:44:33 -0500

    Creating original charts

--- SwiftUICharts/ContentView.swift
+++ SwiftUICharts/ContentView.swift
@@ -4,63 +4,25 @@
 //
 //  Created by David Li on 2024-01-27.
 //
-
 import SwiftUI
-import SwiftData
+import Charts
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+        Chart {
+            BarMark(
+                x: .value("Day", "Monday"),
+                y: .value("Steps", 6019)
+            )
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            BarMark(
+                x: .value("Day", "Tuesday"),
+                y: .value("Steps", 7200)
+            )
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
